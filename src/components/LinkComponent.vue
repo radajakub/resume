67833ca634a753b570d11548a2bc31ac0e7e4969
Jakub Rada <dev.jakubrada@icloud.com>
2023-09-19 23:34:29 +0200

    Add link wrapper component

---
+++ src/components/LinkComponent.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+const props = defineProps<{ url: string, border: boolean }>();
+</script>
+
+<template>
+    <a :href="props.url" target="_blank">
+        <div class="hover-box" :class="{ border: props.border }">
+            <slot></slot>
+        </div>
+    </a>
+</template>
+
+<style scoped>
+a,
+a:visited,
+a:active,
+a:link {
+    text-decoration: none;
+    color: inherit;
+    cursor: auto;
+}
+
+.hover-box {
+    display: block;
+    border-radius: var(--border-radius);
+    height: 50px;
+    cursor: pointer;
+}
+
+.hover-box:hover {
+    background: var(--color-background-hover);
+}
+
+.border {
+    border: var(--border-thickness) solid var(--color-border);
+}
+</style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,19 +1,5 @@
 <script setup lang="ts">
-
 import IntroductionComponent from '@/components/intro/IntroductionComponent.vue';
-import AboutMeComponent from '@/components/intro/AboutMeComponent.vue';
-import SchoolComponent from '@/components/education/SchoolComponent.vue';
-
-import education_json from '@/assets/data/me/education.json';
-const educations = education_json['all'];
-import personal_json from '@/assets/data/me/personal.json';
-const about_me = personal_json['aboutme'];
-
-
-// extract the array of educations from the json
-// const education = education_json['all']
-// ref([]) creates an array which can be modified by v-model
-// to modify ref, use .value property
 </script>
 
 
