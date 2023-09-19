24185c3997e7853245b9059c51844512aaa79d35
Jakub Rada <dev.jakubrada@icloud.com>
2023-09-20 00:00:09 +0200

    Add BadgeComponent

---
+++ src/components/BadgeComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{ border?: boolean }>();
+</script>
+
+<template>
+    <div class="badge" :class="{ border: props.border }">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.badge {
+    display: inline-block;
+    border-radius: var(--border-radius);
+    padding: var(--badge-vertical-padding) var(--badge-horizontal-padding);
+    background: var(--color-badge);
+    color: var(--color-badge-text);
+}
+
+.border {
+    border: var(--border-thickness) solid var(--color-border);
+}
+</style>
