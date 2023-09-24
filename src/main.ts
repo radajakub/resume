7048f5890cc578400cf8ef40d46c47643ce77355
Jakub Rada <dev.jakubrada@icloud.com>
2023-09-24 11:01:16 +0200

    Add icons to skills

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 /* import specific icons */
-import { faEnvelope, faMobile, faMapLocationDot, faCalendarDays, faCakeCandles } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faDiscord, faHackerrank, faKaggle } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faMobile, faMapLocationDot, faCalendarDays, faCakeCandles, faLanguage, faCode, faBrain, faPerson, faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faDiscord, faHackerrank, faKaggle, faAppStoreIos } from '@fortawesome/free-brands-svg-icons';
 
 
 /* add icons to the library */
@@ -22,11 +22,17 @@ library.add(
     faMapLocationDot,
     faCalendarDays,
     faCakeCandles,
+    faLanguage,
+    faCode,
+    faBrain,
+    faPerson,
+    faDumbbell,
     faLinkedin,
     faGithub,
     faDiscord,
     faHackerrank,
     faKaggle,
+    faAppStoreIos,
 );
 
 // create new application instance
