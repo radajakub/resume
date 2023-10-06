92577ec582f3d3a4f5144ef15c534e25ce7f459e
Jakub Rada <dev.jakubrada@icloud.com>
2023-10-07 00:32:05 +0200

    Try to add assetsInclude option

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: [
+    "./src/assets/**/*",
+  ],
   plugins: [
     vue(),
   ],
