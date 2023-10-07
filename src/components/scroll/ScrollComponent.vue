82c258b12542d0ac2e624b044a04cd5383e82769
Jakub Rada <dev.jakubrada@icloud.com>
2023-10-07 14:15:09 +0200

    Add exceptions for null

--- src/components/scroll/ScrollComponent.vue
+++ src/components/scroll/ScrollComponent.vue
@@ -17,7 +17,11 @@ const active = ref(0);
 
 function clickSection(index: number) {
     active.value = index;
-    var y = document.getElementById(links[index]).getBoundingClientRect().top + window.scrollY;
+    var element = document.getElementById(links[index]);
+    if (element == null) {
+        throw new Error("[click] The id of scroll element is null");
+    }
+    var y = element.getBoundingClientRect().top + window.scrollY;
     if (index == 0) {
         y = 0;
     }
@@ -29,7 +33,10 @@ function clickSection(index: number) {
 
 document.addEventListener("scroll", () => {
     for (var i = 0; i < links.length; i++) {
-        var elementBox = document.getElementById(links[i]).getBoundingClientRect();
+        var elementBox = document.getElementById(links[i])?.getBoundingClientRect();
+        if (elementBox == null) {
+            throw new Error("[scroll] The id of scroll element is null");
+        }
         var middle = elementBox.top + elementBox.height / 2;
         if (middle >= 0) {
             active.value = i;
