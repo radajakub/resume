ac6a1056d4978f0d2b4cdb813029875766ccd46b
Jakub Rada <dev.jakubrada@icloud.com>
2023-10-07 13:17:01 +0200

    Add scroll

---
+++ src/components/scroll/ScrollComponent.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import SeparatorComponent from "@/components/SeparatorComponent.vue";
+
+const props = defineProps<{
+    labels: string[],
+}>();
+
+function createLink(label: string) {
+    return label.toLowerCase().replaceAll(" ", "-");
+}
+
+const links = props.labels.map((label) => createLink(label));
+
+const active = ref(0);
+
+function clickSection(index: number) {
+    active.value = index;
+    var y = document.getElementById(links[index]).getBoundingClientRect().top + window.scrollY;
+    if (index == 0) {
+        y = 0;
+    }
+    window.scrollTo({
+        top: y,
+        behavior: "smooth",
+    });
+}
+</script>
+
+<template>
+    <div class="wrapper thin-border rounded-border">
+        <div v-for="(label, index) in props.labels" :key="index">
+            <div class="element" :class="{ active: index == active }" @click="clickSection(index)">
+                <span class="item pv-20">{{ label }}</span>
+            </div>
+            <SeparatorComponent :dotted=true :noVerticalPadding=true v-if="index < props.labels.length - 1" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.element {
+    width: 100%;
+    cursor: pointer;
+}
+
+.element:hover {
+    background-color: var(--color-background-hover);
+    color: var(--button-active-text-color);
+}
+
+.element.active {
+    background-color: var(--color-border);
+    color: var(--color-background);
+}
+
+.wrapper {
+    width: var(--scroll-width);
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    text-align: center;
+}
+
+.item {
+    padding-right: 5;
+    writing-mode: vertical-rl;
+    transform: rotate(180deg);
+}
+</style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -4,42 +4,58 @@ import { inject } from "vue";
 import IntroductionComponent from "@/components/intro/IntroductionComponent.vue";
 import AboutMeComponent from "@/components/about_me/AboutMeComponent.vue";
 import WrapListComponent from "@/components/lists/WrapListComponent.vue";
+import ScrollComponent from "@/components/scroll/ScrollComponent.vue";
 
 import { Data } from "@/data.ts";
 
 const data: Data | undefined = inject("data");
+
+const sections = [
+    "Introduction", "Skills", "Education", "Work", "Projects", "Achievements"
+];
 </script>
 
 
 <template>
+    <div class="scroll-col">
+        <ScrollComponent :labels="sections" />
+    </div>
     <!-- introduction row [personal column, about me column] -->
-    <div class="intro-row mb-50">
-        <IntroductionComponent />
-        <div class="about-me-col ml-20">
-            <AboutMeComponent class="mb-20" />
-            <WrapListComponent :skills="data?.skills" />
+    <div class="ml-50">
+        <div class="intro-row mb-50">
+            <IntroductionComponent id="introduction" />
+            <div class="about-me-col ml-20">
+                <AboutMeComponent id="aboutme" class="mb-20" />
+                <WrapListComponent id="skills" :skills="data?.skills" />
+            </div>
         </div>
-    </div>
 
-    <div class="edu-col mb-50">
-        <WrapListComponent :educations="data?.educationsSorted()" />
-    </div>
+        <div class="edu-col mb-50">
+            <WrapListComponent id="education" :educations="data?.educationsSorted()" />
+        </div>
 
-    <div class="work-col mb-50">
-        <WrapListComponent :works="data?.worksSorted()" />
-    </div>
+        <div class="work-col mb-50">
+            <WrapListComponent id="work" :works="data?.worksSorted()" />
+        </div>
 
-    <div class="project-col mb-50">
-        <WrapListComponent :projects="data?.projectsSorted()" />
-    </div>
+        <div class="project-col mb-50">
+            <WrapListComponent id="projects" :projects="data?.projectsSorted()" />
+        </div>
 
-    <div class="project-col mb-50">
-        <WrapListComponent :achievements="data?.achievementsSorted()" />
+        <div class="project-col mb-50">
+            <WrapListComponent id="achievements" :achievements="data?.achievementsSorted()" />
+        </div>
     </div>
 </template>
 
 
 <style scoped>
+.scroll-col {
+    position: fixed;
+    left: 20px;
+    top: var(--base-padding-vertical);
+}
+
 .intro-row {
     display: flex;
     flex-direction: row;
