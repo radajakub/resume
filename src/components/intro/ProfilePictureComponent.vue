139ca86388406ecae98601332a8fd5e938cd3d4d
Jakub Rada <dev.jakubrada@icloud.com>
2023-11-19 20:18:45 +0100

    Add download cv button to profile picture

--- src/components/intro/ProfilePictureComponent.vue
+++ src/components/intro/ProfilePictureComponent.vue
@@ -1,20 +1,56 @@
 <script setup lang="ts">
 const props = defineProps<{ showBorder: boolean }>();
+
+function showCV() {
+    window.open("resume.pdf", "_blank");
+}
 </script>
 
 <template>
-    <div class="avatar" :class="{ bordered: props.showBorder }"></div>
+    <div class="wrapper" @click="showCV()">
+        <div class="avatar" :class="{ bordered: props.showBorder }"></div>
+        <div class="title inner">Print Resumé</div>
+    </div>
 </template>
 
 <style scoped>
-.avatar {
-    background-image: url("/pictures/profile_photo.jpg");
+.wrapper {
+    position: relative;
     width: var(--profile-picture-size);
     height: var(--profile-picture-size);
+    cursor: pointer;
+}
+
+.avatar {
+    position: absolute;
+    z-index: 1;
+    width: var(--profile-picture-size);
+    height: var(--profile-picture-size);
+    background-image: url("/pictures/profile_photo.jpg");
     background-size: cover;
     background-position: center;
     border-radius: 50%;
     border: none;
+    cursor: pointer;
+    display: flex;
+}
+
+.inner {
+    position: absolute;
+    z-index: 2;
+    text-align: center;
+    top: 50%;
+    transform: translateY(-50%);
+    vertical-align: middle;
+    visibility: hidden;
+}
+
+.wrapper:hover>.avatar {
+    opacity: 0.5;
+}
+
+.wrapper:hover>.inner {
+    visibility: visible;
 }
 
 .bordered {
